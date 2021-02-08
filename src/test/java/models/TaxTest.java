e653ce8c31d73a5cb9f71df6592209ede0f850eb
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-08 21:57:09 +0530

    Add test for tax class

---
+++ src/test/java/models/TaxTest.java
@@ -0,0 +1,37 @@
+package models;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static models.TaxType.SALES;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class TaxTest {
+
+    @Test
+    @DisplayName("Should test get percentage of tax")
+    void shouldTesGetTaxPercentage() {
+        // Arrange
+        BigDecimal taxPercentage = new BigDecimal("12.5");
+        Tax tax = new Tax(SALES, taxPercentage);
+        BigDecimal expectedTaxPercentage = new BigDecimal("12.5");
+
+        // Assert
+        assertThat(tax.getTaxPercentage(), is(expectedTaxPercentage));
+    }
+
+    @Test
+    @DisplayName("Should test equality of two tax")
+    void shouldTestEquals() {
+        // Arrange
+        BigDecimal taxPercentage = new BigDecimal("12.5");
+        Tax tax = new Tax(SALES, taxPercentage);
+        Tax expectedTest = new Tax(SALES, taxPercentage);
+
+        // Assert
+        assertThat(tax, is(expectedTest));
+    }
+}
