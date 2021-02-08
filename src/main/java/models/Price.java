890661b861daa0847edcbe507a9bccc4544da510
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-08 21:48:56 +0530

    Add test for price.java and refactor

--- src/main/java/models/Price.java
+++ src/main/java/models/Price.java
@@ -3,15 +3,27 @@ package models;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import static java.math.BigDecimal.ROUND_HALF_UP;
+
 public class Price {
     private final BigDecimal unitPrice;
     private final Tax tax;
 
     private static final BigDecimal HUNDRED = new BigDecimal("100");
+    private static final Integer PRECISION = 2;
 
     public Price(BigDecimal unitPrice, Tax tax) {
         this.unitPrice = unitPrice;
         this.tax = tax;
+    }
+
+    public BigDecimal getPriceWithTax() {
+        BigDecimal taxPercentage = tax.getTaxPercentage();
+        BigDecimal taxAmount = unitPrice.multiply(taxPercentage).divide(HUNDRED);
+
+        BigDecimal priceWithTax = unitPrice.add(taxAmount);
+
+        return priceWithTax.setScale(PRECISION, ROUND_HALF_UP);
     }
 
     @Override
@@ -25,13 +37,6 @@ public class Price {
     @Override
     public int hashCode() {
         return Objects.hash(unitPrice, tax);
-    }
-
-    BigDecimal getPriceWithTax() {
-        BigDecimal taxPercentage = tax.getTaxPercentage();
-        BigDecimal taxAmount = unitPrice.multiply(taxPercentage).divide(HUNDRED);
-
-        return unitPrice.add(taxAmount);
     }
 
     BigDecimal getUnitPrice() {
---
+++ src/test/java/models/PriceTest.java
@@ -0,0 +1,58 @@
+package models;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static models.ProductType.SOAP;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.math.BigDecimal;
+
+import static models.TaxType.SALES;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceTest {
+    private BigDecimal salesPercentage;
+    private Tax salesTax;
+    private BigDecimal unitPrice;
+    private Price price;
+
+    @BeforeEach
+    void setup() {
+        salesPercentage = new BigDecimal("12.5");
+        salesTax = new Tax(SALES, salesPercentage);
+        unitPrice = new BigDecimal("100.00");
+        price = new Price(unitPrice, salesTax);
+    }
+
+    @AfterEach
+    void tearDown() {
+        salesPercentage = null;
+        salesTax = null;
+        unitPrice = null;
+        price = null;
+    }
+
+    @Test
+    @DisplayName("Should test equality of two price objects")
+    void shouldTestEquals() {
+        // Arrange
+        Price expectedPrice = new Price(unitPrice, salesTax);
+
+        // Assert
+        assertThat(price.equals(expectedPrice), is(true));
+    }
+
+    @Test
+    @DisplayName("Should test get price of product with tax amount")
+    void shouldTestGetPriceWithTax() {
+        // Arrange
+        BigDecimal expectedPrice = new BigDecimal("112.50");
+
+        // Assert
+        assertThat(price.getPriceWithTax(), is(expectedPrice));
+    }
+}
