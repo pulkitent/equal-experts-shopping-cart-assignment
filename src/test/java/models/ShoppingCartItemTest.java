62b4a54ad847712785bb19ce7d064a3ed0191500
Pulkit Gupta <pulkitgupta@Pulkits-MacBook-Pro.local>
2021-02-12 23:04:43 +0530

    Add class structure for ShoppingCartItemTest

---
+++ src/test/java/models/ShoppingCartItemTest.java
@@ -0,0 +1,29 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// TODO : To be implemented here
+class ShoppingCartItemTest {
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void getItemPriceWithTax() {
+    }
+
+    @Test
+    void getTaxAmountOnItem() {
+    }
+
+    @Test
+    void isSameProduct() {
+    }
+
+    @Test
+    void increaseQuantityBy() {
+    }
+}
